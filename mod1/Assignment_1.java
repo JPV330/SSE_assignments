c0b5d9e63e74da5b70aee1c70bd01298e17233b7
JPV330 <89835355+JPV330@users.noreply.github.com>
2021-09-02 03:21:49 -0500

    Update Assignment_1.java

--- mod1/Assignment_1.java
+++ mod1/Assignment_1.java
@@ -4,23 +4,27 @@ public class Assignment_1 {
 
 	public static void main(String[] args) {
 		
+		//Create the first triangle pattern
 		for(int i = 1; i < 5; i++) {
 			for(int j = 0; j < i; j++) {
 				System.out.print("*");
 			}
 			System.out.print(System.lineSeparator());
 		}
+		//Create the line of dots to sperate between triangle patterns
 		for(int a = 0; a < 9; a++) {
 			System.out.print(".");
 		}
 		
+		//Create the line of dots to sperate between triangle patterns
 		System.out.println();
 		for(int a = 0; a < 9; a++) {
 			System.out.print(".");
 		}
 		System.out.println();
 		
-		for(int i = 5; i > 1; i = i - 1) {
+		//Create the inverted triangle pattern
+		for(int i = 5; i > 1; i = i - 1){
 			for(int j = 1; j < i; j++) {
 				System.out.print("*");
 			}
@@ -28,6 +32,7 @@ public class Assignment_1 {
 		}
 		System.out.println();
 		
+		//Create center alinged triangle patern
 		for(int i = 0; i < 4; i++) {
 			for(int j = 5 - i; j > 1; j--) {
 				System.out.print(" ");
@@ -39,16 +44,19 @@ public class Assignment_1 {
 			System.out.println();
 		}
 		
+		//Create dots at bottom of trinagle
 		for(int a = 0; a < 9; a++) {
 			System.out.print(".");
 		}
 		System.out.println();
 		
+		//Create dots at top of center aligned inverted triangle
 		for(int a = 0; a < 9; a++) {
 			System.out.print(".");
 		}
 		System.out.println();
 		
+		//Create center aligned inverted triangle
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < i+1; j++) {
 				System.out.print(" ");
