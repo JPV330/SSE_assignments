2dc4aee9ab15467fd42690d390e26c2e56e603a0
JPV330 <89835355+JPV330@users.noreply.github.com>
2021-09-07 12:45:34 -0500

    Update Mod_2_assignment_2.java

--- mod2/Mod_2_assignment_2.java
+++ mod2/Mod_2_assignment_2.java
@@ -11,44 +11,48 @@ public class Mod_2_assignment_2 {
 		int d2;
 		int entry;
 		int biggest = 0;
-		
+		String location = null;
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Please enter the size of the first dimension of the array : ");
 		d1 = sc.nextInt();
-		
+
 		System.out.print("Please enter the size of the second dimension of the array : ");
 		d2 = sc.nextInt();
-		
-		int [][] user_array = new int [d1][d2];
-		
-		for(int i = 0; i < d1; i++) {
+
+		int[][] user_array = new int[d1][d2];
+
+		for (int i = 0; i < d1; i++) {
 			for (int j = 0; j < d2; j++) {
-				entry = (int)Math.floor(Math.random()*(max-min+1)+min);
+				entry = (int) Math.floor(Math.random() * (max - min + 1) + min);
 				user_array[i][j] = entry;
 			}
 		}
-		
+
 		System.out.println("This is the array created : ");
-		
-		for(int a = 0; a < d1; a++) {
-			for(int b = 0; b < d2; b++) {
+
+		for (int a = 0; a < d1; a++) {
+			for (int b = 0; b < d2; b++) {
 				System.out.print(user_array[a][b] + " ");
 			}
 			System.out.println();
 		}
-		
-		for(int c = 0; c < d1; c++) {
-			for(int d = 0; d < d2; d++) {
-				if(user_array[c][d] > biggest) {
+
+		for (int c = 0; c < d1; c++) {
+			for (int d = 0; d < d2; d++) {
+				if (user_array[c][d] > biggest) {
 					biggest = user_array[c][d];
+					location = "The location is row : " + c + ", column : " + d;
 				}
 			}
 		}
-		
+
+		sc.close();
 		System.out.println();
-		
-		System.out.print("The largest number is : " + biggest);
+
+		System.out.println("The largest number is : " + biggest);
+		System.out.println(location);
 
 	}
 
